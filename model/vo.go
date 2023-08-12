4f5396ae69dc1a381720d12072d6b4226165b365
luo-cheng-xi <chengxi.luo2004@gmail.com>
2023-08-12 18:09:05 +0800

    完善了VO包

--- model/vo.go
+++ model/vo.go
@@ -1,15 +1,34 @@
 package model
 
 type UserVO struct {
-	Id              int64
-	Name            string
-	FollowCount     int64
-	FollowerCount   int64
-	IsFollow        bool
-	Avatar          string
-	BackgroundImage string
-	Signature       string
-	TotalFavorited  int64
-	WorkCount       int64
-	FavoriteCount   int64
+	ID              int64  `json:"id"`
+	Name            string `json:"name"`
+	FollowCount     int64  `json:"follow_count"`
+	FollowerCount   int64  `json:"follower_count"`
+	IsFollow        bool   `json:"is_follow"`
+	Avatar          string `json:"avatar"`
+	BackgroundImage string `json:"background_image"`
+	Signature       string `json:"signature"`
+	TotalFavorited  int64  `json:"total_favorited"`
+	WorkCount       int64  `json:"work_count"`
+	FavoriteCount   int64  `json:"favorite_count"`
+}
+
+type VideoVO struct {
+	ID            int64  `json:"id"`
+	Author        UserVO `json:"author"`
+	PlayUrl       string `json:"play_url"`
+	CoverUrl      string `json:"cover_url"`
+	FavoriteCount int64  `json:"favorite_count"`
+	CommentCount  int64  `json:"comment_count"`
+	IsFavorite    bool   `json:"is_favorite"`
+	Title         string `json:"title"`
+}
+
+type MessageVO struct {
+	ID         int64  `json:"id"`
+	ToUserId   int64  `json:"to_user_id"`
+	FromUserId int64  `json:"from_user_id"`
+	Content    string `json:"content"`
+	CreateTime string `json:"create_time"`
 }
